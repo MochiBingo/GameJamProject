39ca395cda92f33fb442c11bdab76af2d6c97944
MochiBingo <mochibingoshep@gmail.com>
2025-01-25 10:40:32 -0400

    sprint done

--- Assets/CharacterController/PlayerManager.cs
+++ Assets/CharacterController/PlayerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -9,15 +10,13 @@ public class PlayerManager : MonoBehaviour
     public float moveSpeed = 5.0f;
     public float delay = 1.0f;
     private Vector3 moveVector = Vector3.zero;
-    public float sprintMod = 10.0f;
-    public Vector3 jumpHeight = new Vector3 (0, 3f, 0 );
-    public float jumpForce = 3.0f;
+    public float sprintMod = 1.0f;
+    public Vector3 jumpHeight = new Vector3 (0, 2f, 0 );
+    public float jumpForce = 2.0f;
     private bool onGround;
+    private bool isSprinting;
 
-    private void OnCollisionStay()
-    {
-        onGround = true;
-    }
+
 
     void Start()
     {
@@ -33,23 +32,49 @@ public class PlayerManager : MonoBehaviour
     }
     void moveChar(Vector3 moveVector)
     {
-        playerRigidBody.MovePosition(playerRigidBody.position + moveVector * moveSpeed * Time.fixedDeltaTime);
+        playerRigidBody.MovePosition(playerRigidBody.position + (moveVector * moveSpeed * Time.fixedDeltaTime) * sprintMod);
+    }
+    private void OnCollisionEnter()
+    {
+        onGround = true;
+    }
+    private void OnCollisionExit()
+    {
+        onGround = false;
     }
     void jump()
     {
         if (onGround)
         {
             playerRigidBody.AddForce(jumpHeight * jumpForce, ForceMode.Impulse);
-            onGround = false;
+        }
+    }
+    private void Update()
+    {
+        if (onGround && !isSprinting)
+        {
+            sprintMod = 1.0f;
+        }
+        if (onGround && isSprinting)
+        {
+            sprintMod = 2.5f;
         }
     }
     void sprintStart()
     {
-        
+        isSprinting = true;
+        if (onGround)
+        {
+            sprintMod = 2.5f;
+        }    
     }
     void sprintEnd()
     {
-       
+        isSprinting = false;
+        if (onGround)
+        {
+            sprintMod = 1.0f;
+        }
     }
     void updateMoveVector(Vector3 inputVector)
     {
